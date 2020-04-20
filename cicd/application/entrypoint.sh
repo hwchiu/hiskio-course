28bdf27eb2eb2fc671475121fd530a6838ebcb15
Hung-Wei Chiu <hungwei@opennetworking.org>
2020-04-19 17:23:30 -0700

    The prototype of application

---
+++ cicd/application/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+PREFIX="${PREFIX:-default}"
+COMMITHASH="${COMMITHASH:- }"
+COMMITLOG="${COMMITLOG:- }"
+IPS=$(hostname -I)
+
+cat <<EOF > /var/www/html/index.html
+Hostname is ${PREFIX}_${HOSTNAME}
+Latest Commit Hash is ${PREFIX}_${COMMITHASH}
+Latest Commit Log is ${PREFIX}_${COMMITLOG}
+IP address list are ${PREFIX}_${IPS}
+Datetime is $(date)
+EOF
+
+#Generate a new index.html under /var/www/html
+exec $(which nginx) -c /etc/nginx/nginx.conf -g "daemon off;"
