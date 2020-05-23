d2738b1887211ec9dd621ebb373c001fd6bd691b
Hung-Wei Chiu <hungwei@opennetworking.org>
2020-05-23 00:58:02 -0700

    remove unused code

--- cicd/application/tests/init.bash
+++ cicd/application/tests/init.bash
@@ -2,7 +2,6 @@ TYPE=${TYPE:KIND}
 
 if [ "${TYPE}" = "KIND" ];then
     export NODE=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' kind-worker)
-    export PORT=80
 else
     export NODE=127.0.0.1
 fi
