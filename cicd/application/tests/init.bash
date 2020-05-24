bf9bfc9708bad1be759ca4c5b29ef68126c4eb68
Hung-Wei Chiu <hungwei@opennetworking.org>
2020-05-23 21:56:12 -0700

    Support Kubernetes Test

--- cicd/application/tests/init.bash
+++ cicd/application/tests/init.bash
@@ -3,5 +3,5 @@ TYPE=${TYPE:KIND}
 if [ "${TYPE}" = "KIND" ];then
     export NODE=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' kind-worker)
 else
-    export NODE=127.0.0.1
+    export NODE=$(kubectl get nodes minikube -o jsonpath='{.status.addresses[0].address}')
 fi
