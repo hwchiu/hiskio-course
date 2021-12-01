f663781208cd3d86a9258766c1dc385a73bdf57e
Hung-Wei Chiu <hungwei@opennetworking.org>
2021-11-30 23:45:46 -0800

    Fix CI

--- cicd/application/tests/init.bash
+++ cicd/application/tests/init.bash
@@ -2,6 +2,8 @@ TYPE=${TYPE:KIND}
 
 if [ "${TYPE}" = "KIND" ];then
     export NODE=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' kind-worker)
+elif [ "${TYPE}" = "GITHUB-KIND" ];then
+    export NODE=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' chart-testing-control-plane)
 else
     export NODE=$(kubectl get nodes minikube -o jsonpath='{.status.addresses[0].address}')
 fi
